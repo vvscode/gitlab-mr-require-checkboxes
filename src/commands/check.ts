e4cd7c3b2699680d9bff09c4e90f1122ee5458be
Vasil Vanchuk <v.vanchuk@tinkoff.ru>
2024-08-07 11:19:45 +0300

    Update code

---
+++ src/commands/check.ts
@@ -0,0 +1,59 @@
+import { Argv } from 'yargs'
+import { logger } from '../logger'
+
+interface GreetingArgv {}
+
+export const command = 'check'
+export const describe = 'Checks if all checkboxes in Gitlab MR are ticked'
+export const aliases = ['c']
+
+export function builder(yargs: Argv<GreetingArgv>): Argv {
+  return yargs
+}
+
+export async function handler() {
+  const gitlabToken =
+    process.env.GITLAB_API_TOKEN ||
+    logger.prompt('I do not see GITLAB_API_TOKEN environment variable. Please enter it:', { type: 'text' })
+  const projectId =
+    process.env.CI_PROJECT_ID ||
+    logger.prompt('I do not see CI_PROJECT_ID environment variable. Please enter it:', { type: 'text' })
+  const mrId =
+    process.env.CI_MERGE_REQUEST_IID ||
+    logger.prompt('I do not see CI_MERGE_REQUEST_IID environment variable. Please enter it:', { type: 'text' })
+  const gitlabHost =
+    process.env.CI_SERVER_URL ||
+    logger.prompt('I do not see CI_SERVER_URL environment variable. Please enter it:', { type: 'text' })
+
+  const gitlabApiUrl = `${gitlabHost}/api/v4/projects/${projectId}/merge_requests/${mrId}`
+
+  try {
+    const response = await fetch(gitlabApiUrl, {
+      headers: {
+        'Private-Token': gitlabToken,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    // Parse the response as JSON
+    const data = await response.json()
+
+    // Extract MR description
+    const mrDescription = data.description
+    logger.log('Merge Request Description:\n\n', mrDescription)
+
+    if (mrDescription.includes('[ ]')) {
+      logger.log('There are unticked checkboxes')
+      process.exit(1)
+    }
+
+    // Do something with the MR description
+    // For example, you could set it as a CI/CD variable, log it, etc.
+  } catch (error) {
+    logger.error('Error fetching MR details:', error.message)
+    process.exit(1) // Exit with error
+  }
+}
